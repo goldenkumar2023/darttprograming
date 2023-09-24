29c0784ff764e9741d65d97eeedd250f96366a39
DESKTOP-1MV7N3G\LENOVO <golden gdugaon @gmail.com>
2023-09-24 09:34:38 +0530

    first commit

--- lib/Api_colling/postmethod_getdata.dart
+++ lib/Api_colling/postmethod_getdata.dart
@@ -1,17 +1,21 @@
-import 'dart:convert';
-import 'package:http/http.dart'as http;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import '../Model/get_model.dart';
+import '../Model/post_model.dart';
 
+class Postmethod extends StatefulWidget {
+  const Postmethod({Key? key}) : super(key: key);
 
-class HotelScreen extends StatefulWidget {
   @override
-  _HotelScreenState createState() => _HotelScreenState();
+  State<Postmethod> createState() => _PostmethodState();
 }
 
-class _HotelScreenState extends State<HotelScreen> {
-  HotelsModels? hotelData;
+class _PostmethodState extends State<Postmethod> {
+  GetApiModel? getApiModel;
+
   @override
   void initState() {
     super.initState();
@@ -20,17 +24,17 @@ class _HotelScreenState extends State<HotelScreen> {
 
   Future<void> fetchData() async {
     final response = await http.get(
-      Uri.parse('https://hotels4.p.rapidapi.com/locations/v3/search?q=new york&locale=en_US&langid=1033&siteid=300000001'),
+      Uri.parse('https://manage.chaalakya.com/api/c-enquiry-list-client'),
       headers: {
-        "X-RapidAPI-Host": "hotels4.p.rapidapi.com",
-        "X-RapidAPI-Key": "e4adbd2933msh1c2983cc6c7081bp1bf54bjsn1665c629ea60",
+        "response": " response",
+        "message": "message",
       },
     );
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> responseData = json.decode(response.body);
       setState(() {
-        hotelData = HotelsModels.fromJson(responseData);
+        getApiModel = GetApiModel.fromJson(responseData);
       });
     } else {
       throw Exception('Failed to fetch data');
@@ -43,18 +47,17 @@ class _HotelScreenState extends State<HotelScreen> {
       appBar: AppBar(
         title: const Text('Hotel List'),
       ),
-      body:  hotelData != null
-          ?ListView.builder(
-        itemCount: hotelData!.sr.length,
+      body: getApiModel != null
+          ? ListView.builder(
+        itemCount: getApiModel!.sr.length,
         itemBuilder: (context, index) {
-          final location = hotelData!.sr[index];
+          final location = getApiModel!.sr[index];
           return ListTile(
             title: Text(location.regionNames.displayName),
             subtitle: Text(location.regionNames.primaryDisplayName),
             trailing: const Icon(Icons.location_on),
           );
         },
-
       )
           : const Center(
         child: CircularProgressIndicator(),
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutterbasicapp/login_withemail.dart';
-//import 'package:flutterbasicapp/profile.dart';
-//import 'package:flutterbasicapp/splashscreen.dart';
+// import 'package:flutterbasicapp/login_withemail.dart';
+// import 'package:flutterbasicapp/profile.dart';
+// import 'package:flutterbasicapp/splashscreen.dart';
 
 import 'Api_colling/Get_Userdata.dart';
+
 import 'firebase_options.dart';
-//import 'googleAuth/google auth.dart';
-//import 'googlemap/current_location.dart';
+import 'future_builder/Streem_builder/Fatch_data.dart';
+import 'future_builder/get_Data.dart';
+// import 'googleAuth/google auth.dart';
+// import 'googlemap/current_location.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,7 +31,8 @@ class homePage1 extends StatelessWidget {
          theme: ThemeData(
            primarySwatch: Colors.blue,
          ),
-          home: HotelScreen(),
+         home: UserList(),
+      // home: ProductListPage(),
         //homeome: splashscreen(),
         //home: login_withemail(),
       // home: Currentlocation(),
